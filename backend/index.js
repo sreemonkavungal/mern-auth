0ead340282859d492670a17b8ca2d0342e002c86
SREEMON KS <sreemonkavungal@gmail.com>
2024-02-16 20:37:32 +0530

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO).then(() => { 
+    console.log('Connected To MongoDB');
+}).catch(() => {
+    console.log(err);
+})
+
 
 const app = express();
 
