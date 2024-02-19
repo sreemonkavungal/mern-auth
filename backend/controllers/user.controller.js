73ba92e464b6bfb91d8882114180e779a1285a6e
SREEMON KS <sreemonkavungal@gmail.com>
2024-02-19 21:00:49 +0530

    create test API routes

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,7 @@
+
+ const test = (req, res) => {
+    res.json({
+        message: 'API Is Working!'
+    });
+};
+export default test;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from  './routes/user.route.js';
 dotenv.config();
 
 // MongoDB Connection
@@ -16,3 +17,6 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server Listening On Port 3000');
 })
+
+// Create API Route
+app.use('/api/user', userRoutes);
