5e7fffb90704fb2166417398fbae1a726c291ad5
SREEMON KS <sreemonkavungal@gmail.com>
2024-02-19 22:00:36 +0530

    create SignUp API route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from  './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 // MongoDB Connection
@@ -14,9 +15,13 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
+// To add JSON as Input to backend
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server Listening On Port 3000');
 })
 
 // Create API Route
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
