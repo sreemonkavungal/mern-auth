9bd01ff16839ad4ddab0e2480691b2aed0a8a0de
SREEMON KS <sreemonkavungal@gmail.com>
2024-02-20 12:39:47 +0530

    create SignIn API routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from '../models/user.model.js';
 import bcryptjs  from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
+// SignUP
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     // To Hash The Password
@@ -16,3 +19,27 @@ export const signup = async (req, res, next) => {
     }
     
 };
+
+// SignIn
+export const signin = async (req, res, next) => {
+    const {email, password} =  req.body;
+    try {
+        const validUser = await  User.findOne({email});
+        if (!validUser) return next(errorHandler(401,'User Not Found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401,"Invalid Password"));
+        
+    // Create JWT Token
+    const token = jwt.sign({id:validUser._id} ,process.env.JWT_SECRET);
+    const {password: hashedPassword, ...rest} = validUser._doc;
+    // Adding Expiry Date To Cookie
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+    .cookie('access_token',token, {httpOnly: true,  expires: expiryDate })
+    .status(200)
+    .json(rest)
+       
+    } catch (error) {
+        next(error);
+    }
+}
