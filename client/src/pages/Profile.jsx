985468d11b3c181c38907d03aa71c6c47d66ddc7
SREEMON KS <sreemonkavungal@gmail.com>
2024-02-22 23:29:02 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from '../redux/user/userSlice';
 
 export default function Profile() {
@@ -61,6 +64,7 @@ const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
+// Handle Update
 const handleSubmit = async (e) => {
   e.preventDefault();
   try {
@@ -82,7 +86,25 @@ const data = await res.json();
     } catch (error) {
       dispatch(updateUserFailure(error));
     }
-  }; 
+  };
+  
+// Handle Delete
+  const handleDeleteAccount = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error));
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -149,11 +171,15 @@ const data = await res.json();
       </button>
     </form>
     <div className='flex justify-between mt-5'>
-      <span className='text-red-700 cursor-pointer'>Delete Account</span>
+    <span
+      onClick={handleDeleteAccount}
+      className='text-red-700 cursor-pointer'>
+      Delete Account
+    </span>
       <span className='text-red-700 cursor-pointer'>Sign out</span>
     </div>
-    <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
-    <p className='text-green-700 mt-5 justify-center'>
+    <p className='text-red-700 mt-5 flex justify-center'>{error && 'Something went wrong!'}</p>
+    <p className='text-green-700 mt-5 flex justify-center'>
       {updateSuccess && 'User is updated successfully!'}
     </p>
   </div>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -34,6 +34,18 @@ const userSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
       },
+      deleteUserStart: (state) => {
+        state.loading = true;
+      },
+      deleteUserSuccess: (state) => {
+        state.currentUser = null;
+        state.loading = false;
+        state.error = false;
+      },
+      deleteUserFailure: (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      },
     }
 });
 
@@ -43,6 +55,9 @@ export const {
     signInFailure,
     updateUserFailure,
     updateUserStart,
-    updateUserSuccess } = userSlice.actions;
+    updateUserSuccess,
+    deleteUserFailure,
+    deleteUserStart,
+    deleteUserSuccess } = userSlice.actions;
 
 export default userSlice.reducer;
