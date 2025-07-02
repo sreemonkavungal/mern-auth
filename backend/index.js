a4e34131d462c984675e31563268faefdd710eed
SREEMON K S <90373393+sreemonkavungal@users.noreply.github.com>
2025-07-02 12:37:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 // MongoDB Connection
 mongoose.connect(process.env.MONGO).then(() => { 
     console.log('Connected To MongoDB');
-}).catch(() => {
+}).catch((err) => {
     console.log(err);
 })
 
